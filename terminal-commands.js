eeb059108948bd0c1dac766d7c1f858d4ec27383
Sandra Tourjman <sandratourjman@Sandras-MacBook-Pro.local>
2019-03-20 21:32:41 -0500

    First commit

---
+++ terminal-commands.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+module.exports.ls = () => {
+	fs.readdir('./', (err, files) => {
+		const filesToString = files.reduce((acc, file) => {
+			return `${acc} ${file}`;
+		}, '');
+
+		console.log(filesToString);
+	});
+};
+
+module.exports.touch = () => {
+  fs.exists('message.txt', (exists) => {
+  	if (exists) {
+  		console.log("This file already exists");
+  	} else {
+  		fs.writeFile('message.txt', 'Hello Node.js', 'utf8', (err) => {
+  			if (err) throw err;
+  			console.log('file created');
+  		});
+  	}
+  });
+};
+
+module.exports.mkdir = () => {
+  fs.exists('./terminal-commands', (exists) => {
+  	if (exists) {
+  		console.log("This folder already exists");
+  	} else {
+  		fs.mkdir('/terminal-commands', { recursive: true }, (err) => {
+  			if (err) throw err;
+  			console.log('folder created');
+		});
+  	}
+  });
+};
